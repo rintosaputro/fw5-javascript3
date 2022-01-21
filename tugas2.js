d91f058bce3ab7f31e25dafbbfd6414bd86ffa96
rintosaputro <saputrorinto@gmail.com>
2022-01-21 10:07:20 +0700

    delete node_modules

--- tugas2.js
+++ tugas2.js
@@ -5,7 +5,7 @@ const getmonth = (callback) => { // inisialisasi fungsi getmonth dengan paramete
         if(!error) { // validasi apakah error false
             callback(null, month) // jika validasi true parameter callback akan diisi dengan null dan month
         } else { // validasi jika error true
-            callback(new Error('Sorry Data Not Found', [])) // mengisi parameter callback dengan pesan error untuk parameter pertama dan array kosong untuk parameter kedua
+            callback(new Error('Sorry Data Not Found'), []) // mengisi parameter callback dengan pesan error untuk parameter pertama dan array kosong untuk parameter kedua
         }
     }, 4000) // menentukan parameter ke dua setTimeOut untuk menjalankan program callback function pada parameter pertama dengan menunggu 4 detik
 }
@@ -14,22 +14,12 @@ getmonth((err, arr) => { // memanggil fungsi getmonth dengan isi parameter callb
     if(err) { // validasi apakah nilai dari parameter err ada
         console.log(err)   // mencetak err jika true
     } else { // validasi false / jika err tidak null
-        const month = arr.map((item) => console.log(item)) // mengakses nilai dari parameter arr dengan map yang akan mencetak item dari array arr
+        const month = arr.map(item => item) // mengakses nilai dari parameter arr dengan map yang akan mencetak item dari array arr
+        console.log(...month)
     }
 })
 
 /*
 Output:
-January
-February
-March
-April
-May
-Juni
-July
-August
-September
-October
-November
-December
+January February March April May Juni July August September October November December
 */
